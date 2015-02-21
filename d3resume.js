185f249a1df73b0d82d3f24aeddd0124dc53d8fe
Camron Moore <camronbmoore@gmail.com>
2015-02-21 13:49:41 -0700

    Adjusted svg width to be the width of the section to all for use of full screen witdth.

--- d3resume.js
+++ d3resume.js
@@ -1,6 +1,7 @@
 $(function () {
     var margin = {top: 20, right: 120, bottom: 20, left: 120},
-        width = 960 - margin.right - margin.left,
+        sectionWidth = $('section').width(),
+        width = sectionWidth - margin.right - margin.left,
         height = 600 - margin.top - margin.bottom;
 
     var i = 0,
